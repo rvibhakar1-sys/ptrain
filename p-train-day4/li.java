ecc5dcde5474aa67db06efdcc08bae4004f3e121
rvibhakar1-sys <rvibhakar1@gmail.com>
2026-09-19 12:10:41 +0530

    qwerty

---
+++ p-train-day4/li.java
@@ -0,0 +1,11 @@
+package p-train-day4;
+
+public class li {
+    public static void main(String[] args) {
+        LinkedList<Character> li= new LinkedList<>();
+        li.add('a');
+        li.add('b');
+        li.add('c');
+        System.out.println(li);
+    }
+}
---
+++ p-train-day4/pq.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class pq {
+    public static void main(String[] args) {
+            PriorityQueue<Integer> q=  new PriorityQueue()
+            q.add(1);
+            q.add(7);
+            q.add(3);
+            q.add(5);
+            System.out.println(q.poll());
+
+    }
+}
